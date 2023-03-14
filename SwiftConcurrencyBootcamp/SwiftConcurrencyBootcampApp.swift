88e5ff0d9eb1439a9fd87ea77f0213629979123e
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2023-03-14 14:36:40 -0300

    How to use Do, Try, Catch and Throws

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by FABRICIO ALVARENGA on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
